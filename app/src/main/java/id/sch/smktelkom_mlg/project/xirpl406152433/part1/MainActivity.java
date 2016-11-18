eac27aee835e8a238ef79c8ca23b09b9d9303976
Deni Dewanta <didierdeni21@yahoo.com>
2016-11-18 07:11:40 +0700

    Create Sliding tabs for MBOK JAMU

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl406152433/part1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl406152433/part1/MainActivity.java
@@ -1,13 +1,57 @@
 package id.sch.smktelkom_mlg.project.xirpl406152433.part1;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    FragmentAdapterClass fragmentAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar1);
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout1);
+        viewPager = (ViewPager) findViewById(R.id.pager1);
+
+        setSupportActionBar(toolbar);
+
+        tabLayout.addTab(tabLayout.newTab().setText("JAMU"));
+        tabLayout.addTab(tabLayout.newTab().setText("PIJAT"));
+        tabLayout.addTab(tabLayout.newTab().setText("ABOUT US"));
+
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        fragmentAdapter = new FragmentAdapterClass(getSupportFragmentManager(), tabLayout.getTabCount());
+
+        viewPager.setAdapter(fragmentAdapter);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab LayoutTab) {
+
+                viewPager.setCurrentItem(LayoutTab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab LayoutTab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab LayoutTab) {
+
+            }
+        });
     }
 }
