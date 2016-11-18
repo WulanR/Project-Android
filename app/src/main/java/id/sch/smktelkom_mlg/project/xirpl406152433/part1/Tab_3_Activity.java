344204eb058175ea08da9705404398d2267cb769
Deni Dewanta <didierdeni21@yahoo.com>
2016-11-18 07:12:46 +0700

    Create Sliding tabs for MBOK JAMU

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl406152433/part1/Tab_3_Activity.java
@@ -0,0 +1,17 @@
+package id.sch.smktelkom_mlg.project.xirpl406152433.part1;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab_3_Activity extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.activity_tab_3, container, false);
+
+    }
+}
