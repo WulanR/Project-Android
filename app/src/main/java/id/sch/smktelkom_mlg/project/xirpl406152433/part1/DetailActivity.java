422acde2e9311c8be1356a228ae0b70b23510aa5
farisphp <faris.perwira@gmail.com>
2016-11-22 12:26:44 +0700

    Update layout

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl406152433/part1/DetailActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl406152433/part1/DetailActivity.java
@@ -1,8 +1,6 @@
 package id.sch.smktelkom_mlg.project.xirpl406152433.part1;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -34,15 +32,6 @@ public class DetailActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 onBackPressed();
-            }
-        });
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
             }
         });
     }
--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl406152433/part1/JamuAdapter.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl406152433/part1/JamuAdapter.java
@@ -5,16 +5,12 @@ package id.sch.smktelkom_mlg.project.xirpl406152433.part1;
  */
 
 import android.content.Context;
-import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.List;
 
@@ -40,31 +36,14 @@ public class JamuAdapter extends RecyclerView.Adapter<JamuAdapter.MyViewHolder> 
     @Override
     public void onBindViewHolder(final MyViewHolder holder, int position) {
         Jamu jamu = jamuList.get(position);
-        holder.thumbnail.setImageResource(jamu.thumbnail);
+        holder.thumbnail.setImageResource(jamu.getThumbnail());
         holder.title.setText(jamu.getName());
         holder.count.setText(jamu.getBahanpokok() + " bahan dasar pokok");
 
         // loading jamu cover using Glide library
         //Glide.with(mContext).load(jamu.getThumbnail()).into(holder.thumbnail);
 
-        holder.overflow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showPopupMenu(holder.overflow);
-            }
-        });
-    }
 
-    /**
-     * Showing popup menu when tapping on 3 dots
-     */
-    private void showPopupMenu(View view) {
-        // inflate menu
-        PopupMenu popup = new PopupMenu(mContext, view);
-        MenuInflater inflater = popup.getMenuInflater();
-        inflater.inflate(R.menu.menu_jamu, popup.getMenu());
-        popup.setOnMenuItemClickListener(new MyMenuItemClickListener());
-        popup.show();
     }
 
     @Override
@@ -85,7 +64,6 @@ public class JamuAdapter extends RecyclerView.Adapter<JamuAdapter.MyViewHolder> 
             title = (TextView) view.findViewById(R.id.title);
             count = (TextView) view.findViewById(R.id.count);
             thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
-            overflow = (ImageView) view.findViewById(R.id.overflow);
             bahan = (TextView) view.findViewById(R.id.jamu_bahan);
             cara = (TextView) view.findViewById(R.id.jamu_cara);
             deskripsi = (TextView) view.findViewById(R.id.jamu_deskripsi);
@@ -96,29 +74,14 @@ public class JamuAdapter extends RecyclerView.Adapter<JamuAdapter.MyViewHolder> 
                     mIJamuAdapter.doClick(getAdapterPosition());
                 }
             });
+
+            thumbnail.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mIJamuAdapter.doClick(getAdapterPosition());
+                }
+            });
         }
     }
 
-    /**
-     * Click listener for popup menu items
-     */
-    class MyMenuItemClickListener implements PopupMenu.OnMenuItemClickListener {
-
-        public MyMenuItemClickListener() {
-        }
-
-        @Override
-        public boolean onMenuItemClick(MenuItem menuItem) {
-            switch (menuItem.getItemId()) {
-                case R.id.action_add_favourite:
-                    Toast.makeText(mContext, "Ditambahkan Favorit", Toast.LENGTH_SHORT).show();
-                    return true;
-                case R.id.action_read_later:
-                    Toast.makeText(mContext, "Baca nanti", Toast.LENGTH_SHORT).show();
-                    return true;
-                default:
-            }
-            return false;
-        }
-    }
 }
